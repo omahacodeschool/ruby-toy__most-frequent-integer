115f21aeaa4fe547f93e19c70eacef54d72c8d7d
sweetstyle11 <ksandage08@gmail.com>
2016-01-10 20:42:03 +0000

    Done

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -3,5 +3,16 @@
 # exists, return nil.
 
 def find_most_frequent_integer(arr)
-  # put your code here
+    counts = Hash.new 0  #creates new hash 
+        arr.each do |int|
+         counts[int] += 1   
+        end
+max = counts.values.max
+output_hash = Hash[counts.select {|k,v| v == max}]
+   return nil if output_hash.length > 1
+output_hash.keys[0]
 end
+
+find_most_frequent_integer([1, 2, 1, 1])
+find_most_frequent_integer([1, 2, 3, 4])
+find_most_frequent_integer([1, 1, 2, 3, 3])
