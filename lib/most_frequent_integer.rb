96944ebe5718b238e78d57ebf773eec3c3e57e27
maedae <meagan.jungman@gmail.com>
2016-01-17 23:11:29 +0000

    done

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -3,5 +3,29 @@
 # exists, return nil.
 
 def find_most_frequent_integer(arr)
-  # put your code here
+  mean = Hash.new(0)
+  max = 0
+	maxkey = 0
+	is_duplicate = false
+	if arr.empty? 
+		return nil
+	end
+	arr.each do |arr| mean[arr] += 1 
+	end
+	mean.each do |key, value|
+    if value >= max
+      if value > max
+				max = value
+				maxkey = key
+				is_duplicate = false
+			else 
+				is_duplicate = true
+			end
+		end
+	end
+	if is_duplicate 
+		return nil
+	else 	
+		return maxkey
+	end
 end
