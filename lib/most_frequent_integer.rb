bd52118dc2e64f8b7f16dcda5ae64b6079dadeab
amymariewall <amymariewall@gmail.com>
2016-01-20 23:00:50 +0000

    Mode Exercise Response

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -3,5 +3,24 @@
 # exists, return nil.
 
 def find_most_frequent_integer(arr)
-  # put your code here
+    new_array=[]
+    final_array=[]
+    array_to_use=arr.uniq
+      array_to_use.each do |x| 
+      new_array << [arr.count(x), x]
+      final_array=new_array.sort
+ end
+ count_array=[]
+ final_array.each do |x|
+     count_array << x.reverse
+  end
+ just_count_array=[]
+ count_array.each do |x|
+     just_count_array << x.pop
+  end
+ z=just_count_array[-1]
+ res=just_count_array[0..-2].include?(z)
+  if res==false
+     return final_array[-1][1]
+  end
 end
