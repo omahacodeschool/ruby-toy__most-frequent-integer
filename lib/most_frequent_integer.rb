7d678d61c1ecea030f8fcc5e9edb10b8d3f3443c
Patrick Clark <patclark1958@gmail.com>
2016-01-20 00:25:55 +0000

    question

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -3,5 +3,15 @@
 # exists, return nil.
 
 def find_most_frequent_integer(arr)
-  # put your code here
+occurances = Hash.new(0)
+arr.each { |int| occurances[int] +=1}
+#puts occurances
+occur_most = occurances.values.max
+final = occurances.select { |k, v| v == occur_most }.keys
+#puts final
+if final.length == 1
+      return final
+   else
+      return nil
+end
 end
