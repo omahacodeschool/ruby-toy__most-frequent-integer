7f525894e5d154800b2c6337d1aeb2a0b3d6c998
Michael Meier <mmeier61@yahoo.com>
2016-01-22 05:18:57 +0000

    So close...but so far

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -3,5 +3,8 @@
 # exists, return nil.
 
 def find_most_frequent_integer(arr)
-  # put your code here
+    num_val = Hash.new(0)                   #This is the new empty hash
+    arr.each {|arr| num_val[arr] += 1}      #Here we iterate over the array, puting it into a hash with the key being the number and the value how many
+    num_val.key(num_val.values.max)         #Here we iterate over the hash and find the key with the highest value
 end
+find_most_frequent_integer(arr)
