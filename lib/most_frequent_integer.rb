0e4fc196cc71f58c02fc0555df5256b82eec19e0
bbiehl <bradley.biehl@gmail.com>
2016-01-22 05:02:02 +0000

    Done

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -3,58 +3,18 @@
 # exists, return nil.
 
 def find_most_frequent_integer(arr)
-  # put your code here
+    hsh = Hash.new(0) #creates a new, empty hash
+    arr.each { |i| hsh[i] += 1 }
+    mode_arr = [] # an empty Array to stash key(s) with the highest frequency
+    hsh.each { |k,v| mode_arr << k if v == hsh.values.max }
+    
+    if mode_arr.length > 1
+        return nil
+    else
+        return mode_arr
+    end
+    
 end
 
-# Here's what I have so far...
-# Example Array = [1, 1, 2, 2, 3, 4, 5]
-
-arr = [1, 1, 2, 2, 3, 4, 5]
-arr.group_by { |i| i } #=> {1=>[1, 1], 2=>[2, 2], 3=>[3], 4=>[4], 5=>[5]}
-
-#or
-
-arr = [1, 1, 2, 2, 3, 4, 5]
-hsh = arr.group_by { |i| i } #=> {1=>[1, 1], 2=>[2, 2], 3=>[3], 4=>[4], 5=>[5]}
-
-# I can get the Array into a Hash, but am not able to return the key with the most values or nil.
-
-# -------------------------------------------------------
-
-arr = [1, 1, 2, 2, 3, 3, 5]
-arr.max_by { |i| arr.count(i) } #=> 1
-
-#or
-
-arr = [1, 1, 2, 2, 3, 4, 5]
-arr.each.max_by { |i| arr.count(i) } #=> 1
-
-# Not what we want
-# For this example returns =>1 instead of =>nil.  
-# Will also return =>1 for an array = [1, 2, 3, 4, 5]
-
-# -----------------------------------------------------
-
-arr = [1, 1, 2, 2, 3, 4, 5]
-arr.uniq.select { |i| arr.count(i) > 1 } #=> [1, 2]
-
-#or
-
-arr = [1, 1, 2, 2, 3, 4, 5]
-brr = arr.uniq #=> [1, 2, 3, 4, 5]
-brr.select { |i| arr.count(i) > 1 } #=> [1, 2]
-
-# Again, not what we want.
-
-# ----------------------------------------------------
-
-# We know this...
-
-arr = [1, 1, 2, 2, 3, 4, 5]
-arr.count(1) #=> 2
-arr.count(2) #=> 2
-arr.count(3) #=> 1
-arr.count(4) #=> 1
-arr.count(5) #=> 1
-
-# I am not yet able to get this to return the most frequent integer
+arr = [1, 2, 3, 4, 4]
+find_most_frequent_integer(arr)
