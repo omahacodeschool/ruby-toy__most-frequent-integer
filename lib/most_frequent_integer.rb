5b581760ad21bc2d754cc3bc4d66d9950ed72fa6
Patrick Clark <patclark1958@gmail.com>
2016-01-22 01:29:45 +0000

    2nd try done

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -8,10 +8,9 @@ arr.each { |int| occurances[int] +=1}
 #puts occurances
 occur_most = occurances.values.max
 final = occurances.select { |k, v| v == occur_most }.keys
-#puts final
-if final.length == 1
-      return final
+   if final.length == 1
+      return final[0]
    else
       return nil
-end
+   end
 end
