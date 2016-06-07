84f5d1fd9c3d948a2466606c6ad159bc51e3246c
Josh Salyer <joshsalyer23@gmail.com>
2016-06-07 19:08:40 +0000

    WIPgit add . Find the Mode

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -3,5 +3,28 @@
 # exists, return nil.
 
 def find_most_frequent_integer(arr)
-  # put your code here
+  #use a Hash to keep track of the number of duplicates
+  new_hash = Hash.new(0)
+  #iterate through each element in the array
+  arr.each do |idx|
+      #add all elements to the new_hash and add one to the key's value if there's a duplicate
+      new_hash[idx] += 1
+  end
+  #make sure that the Array passed in contained something
+  if new_hash.empty?
+      return nil
+  end
+  #----------------------------------------------------------------
+  #Do more iteration to check if one value is greater than the rest??? 
+  #----------------------------------------------------------------
+  #iterate through each key and value in new_hash
+  new_hash.each do |x, y|
+      #if the value is greater than the key then that key had duplicates in the original Array
+      if y > x
+          #return the value that appeared more than once
+          return x
+      else
+          return nil
+      end
+  end
 end
