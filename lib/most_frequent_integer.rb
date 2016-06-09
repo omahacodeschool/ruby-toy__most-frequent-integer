f9f62446b051fdab46530ff891a5f39710a1b6b1
John <CodeMorrow@yahoo.com>
2016-06-09 03:41:54 +0000

    Most Frequent Integer

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -3,5 +3,18 @@
 # exists, return nil.
 
 def find_most_frequent_integer(arr)
-  # put your code here
+    most = []
+    freq = Hash.new(0)
+        arr.each do |i|
+           freq[i] += 1
+        end
+    freq.each do |k,v|
+        if v == freq.values.max
+            most.push(k)
+        end
+    end
+    if most.count > 1
+        nil
+    else return most[0]
+    end
 end
