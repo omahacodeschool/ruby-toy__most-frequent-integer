215387f62f7deb1107503f244e27878ec9e8e9da
Colin Narans <cnarans@gmail.com>
2016-06-11 04:29:54 +0000

    submitting ruby_int

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -3,5 +3,15 @@
 # exists, return nil.
 
 def find_most_frequent_integer(arr)
-  # put your code here
+    highest = nil
+    count = 0
+    for i in 0..arr.length-1
+        if arr.count(arr[i])>count
+            highest = arr[i]
+            count = arr.count(arr[i])
+        elsif (arr.count(arr[i])==count)&&arr[i]!=highest
+            highest=nil
+        end
+    end
+    return highest
 end
