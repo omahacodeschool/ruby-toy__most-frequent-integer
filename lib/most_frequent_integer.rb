73f546cd7583ae0d575a5c271ef475d111560a38
Matt Hughes <mhughes27@gmail.com>
2016-06-12 01:10:37 +0000

    Submitting Mode - WIP

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -4,4 +4,13 @@
 
 def find_most_frequent_integer(arr)
   # put your code here
+  
+#Algorothmic Process
+
+#Create a new array that each integer (eliminate the repeats)
+#Create a hash that pairs each element of the new array with how manys times it appears in the original array
+#Determine the key-value pair with the larget value
+#If the value is unique, return its key
+#Else return nil
+
 end
