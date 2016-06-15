884d51446a49bcf70c8967899a931205d2dab389
Michelle Cali <mctexx@gmail.com>
2016-06-15 23:09:31 +0000

    Submitting code

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -3,5 +3,26 @@
 # exists, return nil.
 
 def find_most_frequent_integer(arr)
-  # put your code here
+    array = arr.sort
+    unique_integers = array.uniq
+    if array === unique_integers
+    elsif arr.length < 2
+        mode = []
+    else
+        integer_frequency = []
+        top_two = []
+        unique_integers.each do |i|
+            array.count(i)
+            integer_frequency << array.count(i)
+        end
+        top_two = integer_frequency.max(2)
+        if top_two[0] === top_two[1]
+            mode = []
+        else
+            a = []
+            a = integer_frequency.index { |i| i === integer_frequency.max }
+            mode = unique_integers[a]  
+        end
+    end
+    return mode
 end
